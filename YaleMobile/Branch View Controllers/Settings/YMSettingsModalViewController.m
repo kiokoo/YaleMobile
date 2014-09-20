bf080b5b4885cc25d81c86a7f3ec1ac76a9f1257
hengchu <hengchu.zhang@gmail.com>
2014-09-20 00:57:51 -0400

    styling and code clean up in settingsmodalvc

--- YaleMobile/Branch View Controllers/Settings/YMSettingsModalViewController.m
+++ YaleMobile/Branch View Controllers/Settings/YMSettingsModalViewController.m
@@ -12,6 +12,9 @@
 #import <PureLayout/PureLayout.h>
 
 @interface YMSettingsModalViewController () <UITableViewDataSource>
+/**
+ *  The text displayed above the name textfield.
+ */
 @property (nonatomic, strong) NSString *welcomeText;
 @end
 
@@ -30,44 +33,58 @@
 {
   [super viewDidLoad];
   
-  UIButton *confirm = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 16, 16)];
-  [confirm setBackgroundImage:[UIImage imageNamed:@"button_navbar_ok.png"] forState:UIControlStateNormal];
-  [self.navigationItem setRightBarButtonItem:[[UIBarButtonItem alloc] initWithCustomView:confirm]];
-  [confirm addTarget:self action:@selector(confirm:) forControlEvents:UIControlEventTouchUpInside];
+  self.welcomeText = @"Hey there! Thank you for using YaleMobile. What would you like me to call you?";
   
+  UIButton *confirm = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 16, 16)];
+  
+  [confirm setBackgroundImage:[UIImage imageNamed:@"button_navbar_ok.png"]
+                     forState:UIControlStateNormal];
+  [self.navigationItem
+      setRightBarButtonItem:[[UIBarButtonItem alloc] initWithCustomView:confirm]];
+  
+  [confirm addTarget:self
+              action:@selector(confirm:)
+    forControlEvents:UIControlEventTouchUpInside];
+
   UIButton *cancel = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 16, 16)];
-  [cancel setBackgroundImage:[UIImage imageNamed:@"button_navbar_cancel.png"] forState:UIControlStateNormal];
-  [self.navigationItem setLeftBarButtonItem:[[UIBarButtonItem alloc] initWithCustomView:cancel]];
-  [cancel addTarget:self action:@selector(cancel:) forControlEvents:UIControlEventTouchUpInside];
+  [cancel setBackgroundImage:[UIImage imageNamed:@"button_navbar_cancel.png"]
+                    forState:UIControlStateNormal];
+  
+  [self.navigationItem
+      setLeftBarButtonItem:[[UIBarButtonItem alloc] initWithCustomView:cancel]];
+  
+  [cancel addTarget:self
+             action:@selector(cancel:)
+   forControlEvents:UIControlEventTouchUpInside];
 #warning TODO(hengchu): this may need to be removed.
   self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg.png"]];
+  
   self.background1.image = [[UIImage imageNamed:@"shadowbg.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(10, 10, 10, 10)];
   self.background1.alpha = 0.6;
-  self.textField1.autocapitalizationType = UITextAutocapitalizationTypeWords;
-  
-  NSString *name = [[NSUserDefaults standardUserDefaults] objectForKey:@"Name"];
-  if (name) self.textField1.text = name;
-  
 
+  
+  NSString *name       = [[NSUserDefaults standardUserDefaults] objectForKey:@"Name"];
+  self.textField1.text = (name) ? (name) : (@"");
   
   [self.navigationController.navigationBar setBarTintColor:[YMTheme blue]];
   
-  self.tableView.dataSource = self;
+  self.tableView.dataSource     = self;
   self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-  self.tableView.scrollEnabled = NO;
+  self.tableView.scrollEnabled  = NO;
   
-  self.textField1.textColor = [YMTheme gray];
+  self.textField1.autocapitalizationType = UITextAutocapitalizationTypeWords;
+  self.textField1.textColor       = [YMTheme lightGray];
+  self.textField1.font            = [UIFont fontWithName:@"HelveticaNeue-Light" size:16.0f];
   self.textField1.backgroundColor = [UIColor clearColor];
-  
-  self.welcomeText = @"Hey there! Thank you for using YaleMobile. What would you like me to call you?";
 }
 
 - (void)confirm:(id)sender
 {
   if (self.textField1.text.length) {
     [[NSUserDefaults standardUserDefaults] setObject:self.textField1.text forKey:@"Name"];
-  } else
+  } else {
     [[NSUserDefaults standardUserDefaults] setObject:nil forKey:@"Name"];
+  }
   [self.navigationController dismissViewControllerAnimated:YES completion:nil];
 }
 
@@ -76,25 +93,10 @@
   [self.navigationController dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (void)didReceiveMemoryWarning
-{
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
-}
-
 - (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
 {
   cell.selectionStyle  = UITableViewCellSelectionStyleNone;
-
-}
-
-- (void)tableView:(UITableView *)tableView willDisplayHeaderView:(UIView *)view forSection:(NSInteger)section
-{
-  //Format header so it's not all caps
-  if([view isKindOfClass:[UITableViewHeaderFooterView class]]){
-    UITableViewHeaderFooterView *tableViewHeaderFooterView = (UITableViewHeaderFooterView *) view;
-    tableViewHeaderFooterView.textLabel.text = [tableViewHeaderFooterView.textLabel.text capitalizedString];
-  }
+  cell.backgroundColor = [UIColor clearColor];
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
@@ -105,11 +107,11 @@
   
   UILabel *label = [UILabel newAutoLayoutView];
   [headerView addSubview:label];
-  [label autoAlignAxis:ALAxisHorizontal toSameAxisOfView:label.superview withOffset:10];
+  [label autoAlignAxis:ALAxisHorizontal toSameAxisOfView:label.superview withOffset:5];
   [label autoAlignAxisToSuperviewAxis:ALAxisVertical];
   [label autoSetDimensionsToSize:CGSizeMake(width, height)];
   
-  label.font = [UIFont systemFontOfSize:14.0];
+  label.font = [UIFont fontWithName:@"HelveticaNeue" size:17.0f];
   label.textColor = [YMTheme gray];
   label.text = self.welcomeText;
   label.numberOfLines = 0;
@@ -119,8 +121,10 @@
 
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
 {
-  CGSize size = [YMGlobalHelper boundText:self.welcomeText withFont:[UIFont systemFontOfSize:14.0] andConstraintSize:CGSizeMake(self.textField1.bounds.size.width, 99999.0)];
-  return size.height + 10;
+  CGSize size = [YMGlobalHelper boundText:self.welcomeText
+                                 withFont:[UIFont fontWithName:@"HelveticaNeue" size:17.0f]
+                        andConstraintSize:CGSizeMake(self.textField1.bounds.size.width, 99999.0)];
+  return size.height + 15;
 }
 
 @end
